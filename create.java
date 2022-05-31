f205bb3cd7f46e9347901345228741111016296d
myw0 <myw9017@gmail.com>
2022-05-31 21:04:47 +0900

    회원 생성

---
+++ create.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class create 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		ArrayList name = new ArrayList();
+		
+		ArrayList password = new ArrayList();
+		String na, nu;
+		boolean bool = true;
+		
+		while(bool) 
+		{
+            System.out.println("1.회원등록");
+            System.out.println("2.종료");
+
+            // 보기를 선택받음
+            int input = sc.nextInt();
+            switch(input) {
+            case 1:
+                System.out.println("등록할 아이디를 입력하세요");
+                na = sc.next();
+             
+                if(name.indexOf(na) == -1) 
+                {
+                    name.add(na);
+                    System.out.println("등록할 비밀번호를 입력하세요");
+                    nu = sc.next();
+                   
+                    password.add(nu);
+                    System.out.println("등록된 아이디는 : " + na + " / 비밀번호는 : " + nu + "입니다.");
+                  
+                
+                } else 
+                {
+                    System.out.println("중복된 아이디가 있습니다.");
+                }
+                break;
+            case 2:
+              
+                System.out.println("종료하겠습니다.");
+                bool = false;
+      
+        
+          
+            }
+        }
+    }
+}
